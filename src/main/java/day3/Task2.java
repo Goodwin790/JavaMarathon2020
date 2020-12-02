b634d5ccbced4f06c2b025324d6f4b07580511de
Goodwin790 <75224239+Goodwin790@users.noreply.github.com>
2020-12-03 00:28:36 +0300

    Day 3 tasks

--- src/main/java/day3/Task2.java
+++ src/main/java/day3/Task2.java
@@ -1,7 +1,18 @@
 package day3;
 
+import java.util.Scanner;
+
 public class Task2 {
     public static void main(String[] args) {
+        Scanner d = new Scanner(System.in);
+        for (; ; ) {
+            double a = d.nextDouble();
+            double b = d.nextDouble();
+            if (b == 0) {
+                break;
+            }
+            System.out.println(a / b);
+        }
 
     }
 }
