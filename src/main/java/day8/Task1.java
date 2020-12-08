1611ce4efc08fe6d3994bb40eac5ed776aeca2e5
Goodwin790 <75224239+Goodwin790@users.noreply.github.com>
2020-12-09 02:47:16 +0300

    Day 8 tasks

--- src/main/java/day8/Task1.java
+++ src/main/java/day8/Task1.java
@@ -1,7 +1,26 @@
 package day8;
 
+import org.w3c.dom.ls.LSOutput;
+
 public class Task1 {
     public static void main(String[] args) {
+        long start = System.currentTimeMillis();
+//        String s = " ";
+//        for (int i = 0; i <= 20000; i++) {
+//            s = s + i + " ";
+//        }
+//        System.out.println(s);
 
+
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i <= 20000; i++) {
+            sb.append(i + " ");
+        }
+        System.out.println(sb.toString());
+        long finish = System.currentTimeMillis();
+        long timeConsumedMillis = finish - start;
+        System.out.println(timeConsumedMillis);
     }
+
 }
